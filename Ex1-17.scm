ab8b3849d91a8f727d9317578180f300ff6369fe
Michael Young <mct25@st-andrews.ac.uk>
2025-09-05 12:06:14 +0100

    1.17

---
+++ Ex1-17.scm
@@ -0,0 +1,14 @@
+;; Elemental operations
+(define (double x) (* x 2))
+(define (halve x) (/ x 2))
+
+;; Logarithmic multiplication
+(define (fast-mult a b)
+  (cond ((= b 0) 0)
+        ((even? b) (fast-mult (double a) (halve b)))
+        (else (+ a (fast-mult a (- b 1))))))
+
+;; Test
+(display
+ (list (* 5 7)
+       (* 100 101)))
