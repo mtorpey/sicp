7acf90cdb07de8e4235d60530ceea76cca097de7
Michael Young <mct25@st-andrews.ac.uk>
2025-11-20 09:30:02 +0000

    2.28

---
+++ 2-28.scm
@@ -0,0 +1,8 @@
+(define (fringe tree)
+  (cond ((null? tree) (list))
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree)) (fringe (cdr tree))))))
+
+(display (fringe '(1 3 (5 7) 9)))
+(display (fringe '((7))))
+(display (fringe '(1 (2 (3 (4 (5 (6 7))))))))
